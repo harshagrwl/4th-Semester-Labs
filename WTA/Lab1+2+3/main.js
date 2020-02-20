999e202a19222973ec7b335fce7860c30abe4cd1
harsh <agrawal.harsh14@gamil.com>
2020-02-20 10:49:49 +0530

    Added WTA Lab4 files

--- WTA/Lab1+2+3/main.js
+++ WTA/Lab1+2+3/main.js
@@ -58,3 +58,51 @@ function login() {
 		alert('Login Unsuccessful. Please Enter a valid Username or Password')
 	}
 }
+
+
+function setCookie(cname,cvalue) {
+  var d = new Date();
+  // d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  // var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";path=/";
+}
+let countc = 0;
+function getCookie(cname,count) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  count++;
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+
+  return "";
+}
+
+var numLoads = parseInt(getCookie('pageLoads'), 10);
+
+if (isNaN(numLoads) || numLoads <= 0) { setCookie('pageLoads', 1); }
+else { setCookie('pageLoads', numLoads + 1); }
+
+var x = getCookie('pageLoads');
+
+function namecheck() {
+	
+	
+  var user=getCookie("username",countc);
+  if (user != "") {
+    alert("Welcome again " + user + " .Thanks to visit again." + " You have vissited this Website " + x + " times.");
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       document.cookie = "username" + "=" + user + ";" + ";path=/";
+     }
+  }
+}
+
